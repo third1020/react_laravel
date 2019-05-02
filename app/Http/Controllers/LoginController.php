4a5e1a35d5ff851f8cefde22ab0e25cde6733c07
thirdppp <thirdppp@gmail.com>
2019-05-03 05:36:42 +0700

    take hash of password off

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -79,9 +79,9 @@ class LoginController extends Controller
 
 if ($validator->fails()) {
 
-    $checkpassword = Hash::check($password, $passworddatabase);
+    // $checkpassword = Hash::check($password, $passworddatabase);
 
-      if($checkpassword){
+      if($password == $passworddatabase){
 
         return $userdatabase->tojson();
 
