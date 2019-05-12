003856b1ba1a54dab77e69bef20c68bd190129ff
botthird <34233131+third1020@users.noreply.github.com>
2019-05-12 14:12:38 +0700

    fix mt_random

--- database/factories/EquipmentFactory.php
+++ database/factories/EquipmentFactory.php
@@ -10,7 +10,7 @@ $factory->define(Equipment::class, function (Faker $faker) {
       'equipment_name'         => $faker->firstNameMale,
       'equipment_detail'       => $faker->bs,
       'equipment_number'       => mt_rand(0000000,99999999),
-      'contact_detail'         => mt_rand(0000000000000,9999999999999),
+      'contact_detail'         => mt_rand(0000000,99999999),
       'equipment_type_id'      => mt_rand(1,50),
       'created_by'             => $faker->firstNameMale,
       'equipment_image'        =>$faker->imageUrl($width = 1920, $height = 1080, 'cats'),
