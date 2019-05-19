c8746eafd9264ff7681d116bdfed96368bfa1b5f
botthird <34233131+third1020@users.noreply.github.com>
2019-05-19 18:51:16 +0700

    fix hash

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
@@ -14,7 +15,7 @@ $factory->define(User::class, function (Faker $faker) {
         'phone_number'      => rand(000000,999999),
         'permission_id'           => rand(1,50),
         'image'             => $faker->imageUrl($width = 1920, $height = 1080, 'cats'),
-        'password'          => '$2y$10$0yRiTWnNI6RROm/icG2Um.8M0pz8fPtcX59jKjGujWA1dkFigjku.', // secret
+        'password'          => Hash::make("12345678")
 
     ];
 });
