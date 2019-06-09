46dd3dbf60df5180b9194d3a81abe307f239a9de
reworld <reworldt@gmail.com>
2019-06-09 18:09:21 +0700

    fix file app.js เรื่อง router group by function

--- cilent/src/App.js
+++ cilent/src/App.js
@@ -4,86 +4,53 @@ import './css/App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './js/components/auth/Login'
 import Dashboard from './js/components/Dashboard'
+import UserIndex from './js/components/User/index'
+import E404 from './js/components/Error/E404'
 import Test from './js/components/Test'
 
-// import ManageUser from './js/components/ManageUser/list_user'
-// import ManagePermission from './js/components/ManagePermission/list_permission'
-// import ManageNews from './js/components/ManageNews/list_news'
-// import ManageNewsType from './js/components/ManageNewsType/list_news_type'
-// import ManageMessage from './js/components/ManageMessage/list_message'
-// import ManageEquipment from './js/components/ManageEquipment/list_equipment'
-// import ManageEquipmentRegister from './js/components/ManageEquipmentRegister/list_equipment_register'
-// import ManageEquipmentType from './js/components/ManageEquipmentType/list_equipment_type'
-// import ManageRequipment from './js/components/ManageRequipment/list_request'
-// import ManageProblem from './js/components/ManageProblem/list_problems'
-// import ManageIncident from './js/components/ManageIncident/list_incident'
-// import ManageContact from './js/components/ManageContact/list_contact'
-// import ManageImpact from './js/components/ManageImpact/list_impact'
-// import ManagePriority from './js/components/ManagePriority/list_priority'
-// import ReportIncident from './js/components/Report/Report_incident'
-// import ReportProblems from './js/components/Report/Report_problems'
-// import ReportRequest from './js/components/Report/Report_request'
 
+class App extends Component {
+  UserRoutes() {
+    return(
+      <Switch>
+        <Route exact path="/User" component={UserIndex} />
+      </Switch>
+    )
+  }
 
-// <Route path='/ManageUser' component={ManageUser} />
-// <Route path='/ManagePermission' component={ManagePermission} />
-// <Route path='/ManageNews' component={ManageNews} />
-// <Route path='/ManageNewsType' component={ManageNewsType} />
-// <Route path='/ManageMessage' component={ManageMessage} />
-// <Route path='/ManageEquipment' component={ManageEquipment} />
-// <Route path='/ManageEquipmentRegister' component={ManageEquipmentRegister} />
-// <Route path='/ManageEquipmentType' component={ManageEquipmentType} />
-// <Route path='/ManageRequipment' component={ManageRequipment} />
-// <Route path='/ManageProblem' component={ManageProblem} />
-// <Route path='/ManageIncident' component={ManageIncident} />
-// <Route path='/ManageContact' component={ManageContact} />
-// <Route path='/ManageImpact' component={ManageImpact} />
-// <Route path='/ManagePriority' component={ManagePriority} />
-// <Route path='/ManageSolution' component={ManageSolution} />
-// <Route path='/ReportIncident' component={ReportIncident} />
-// <Route path='/ReportProblems' component={ReportProblems} />
-// <Route path='/ReportRequest' component={ReportRequest} />
+  LoginRoutes() {
 
+  }
 
+  MainRoutes() {
+    return (
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route path='/Dashboard' component={Dashboard} />
+        <Route path='/Permission' component={Dashboard} />
+        <Route path='/News' component={Dashboard} />
+        <Route path='/Equipment' component={Dashboard} />
+        <Route path='/Req' component={Dashboard} />
+        <Route path='/Report' component={Dashboard} />
+      </Switch>
+    )
+  }
 
-    class App extends Component {
-      render () {
-        return (
-          <BrowserRouter>
-                    <div>
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" component={this.MainRoutes} />
+            <Route path="/User" component={this.UserRoutes} />
+            <Route path="/404" component={E404} />
+          </Switch>
+        </div>
+      </BrowserRouter>
 
-                      <Switch>
-
-                        <Route exact path='/' component={Login} />
-                        <Route path='/Dashboard' component={Dashboard} />
-
-                        <Route path='/ManageUser' component={Dashboard} />
-                        <Route path='/ManagePermission' component={Dashboard} />
-                        <Route path='/ManageNews' component={Dashboard} />
-                        <Route path='/ManageNewsType' component={Dashboard} />
-                        <Route path='/ManageMessage' component={Dashboard} />
-                        <Route path='/ManageEquipment' component={Dashboard} />
-                        <Route path='/ManageEquipmentRegister' component={Dashboard} />
-                        <Route path='/ManageEquipmentType' component={Dashboard} />
-                        <Route path='/ManageRequipment' component={Dashboard} />
-                        <Route path='/ManageProblem' component={Dashboard} />
-                        <Route path='/ManageIncident' component={Dashboard} />
-                        <Route path='/ManageContact' component={Dashboard} />
-                        <Route path='/ManageImpact' component={Dashboard} />
-                        <Route path='/ManagePriority' component={Dashboard} />
-                        <Route path='/ManageSolution' component={Dashboard} />
-                        <Route path='/ReportIncident' component={Dashboard} />
-                        <Route path='/ReportProblems' component={Dashboard} />
-                        <Route path='/ReportRequest' component={Dashboard} />
-
-
-                      </Switch>
-                    </div>
-                  </BrowserRouter>
-
-        )
-      }
-    }
+    )
+  }
+}
 
 
 
---
+++ cilent/src/js/components/Error/E404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class E404 extends React.Component {
+    render() {
+        return (
+            <div><p>404 Page Not Found</p></div>
+        )
+    }
+}
+
+export default E404
